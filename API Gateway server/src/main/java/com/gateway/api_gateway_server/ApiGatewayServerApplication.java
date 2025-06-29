dc514db11abf8ebce259703586f3f8e93ea352a2
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2025-06-29 18:23:54 +0200

    Add Gateway Api server

---
+++ API Gateway server/src/main/java/com/gateway/api_gateway_server/ApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.api_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServerApplication.class, args);
+    }
+
+}
